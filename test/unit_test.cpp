609423406e04b340aea5d038fdba90274de6fe40
ajit jain <ajit.infotech@gmail.com>
2012-11-22 02:03:18 -0600

    changed unit test to include variables instead of constants

--- test/unit_test.cpp
+++ test/unit_test.cpp
@@ -16,13 +16,15 @@ int main()
 	Graph<int> g1;
 	int n1[6];	//node_ordinals
 	int e1[7];	//edge_ordinals
+
+	int a = 1, b = 2, c = 3, d = 4, e = 5,  f = 6;
 	
-	n1[0] = g1.add_node(1);
-	n1[1] = g1.add_node(2);
-	n1[2] = g1.add_node(3);
-	n1[3] = g1.add_node(4);
-	n1[4] = g1.add_node(5);
-	n1[5] = g1.add_node(6);
+	n1[0] = g1.add_node(a);
+	n1[1] = g1.add_node(b);
+	n1[2] = g1.add_node(c);
+	n1[3] = g1.add_node(d);
+	n1[4] = g1.add_node(e);
+	n1[5] = g1.add_node(f);
 
 	e1[0] = g1.add_edge(n1[0], n1[1]);
 	e1[1] = g1.add_edge(n1[0], n1[4]);
