95d23317a89a1236be58337676f6a60153494467
Bharathi <bharathi@bharathi-Inspiron-N5010.(none)>
2012-12-05 23:35:48 -0600

    changes

--- test/unit_test.cpp
+++ test/unit_test.cpp
@@ -14,7 +14,7 @@
 #include "path.h"
 
 void test_topological_sort() {
-    std::cout << "Testing topological sort\n";
+     std::cout << "Testing topological sort\n";
     lib::DirectedGraph<int> g1;
     int n1[6]; //node_ordinals
     int e1[7]; //edge_ordinals
@@ -175,64 +175,10 @@ void test_bipartite_digraph() {
     e1[4] = g1.add_edge(n1[2], n1[3]);
     e1[5] = g1.add_edge(n1[2], n1[5]);
 
-    std::map<int, int> nodemap = lib::is_bipartite(g1);
-    for (auto it = nodemap.begin(); it != nodemap.end(); it++)
-        std::cout << (*it).first << " => " << (*it).second << std::endl;
+    bool isbipartite = lib::is_bipartite(g1);
 }
 
-void test_bipartite_undigraph() {
-    std::cout << "testing bipartite on undirected graph\n";
-    lib::UndirectedGraph<int> g1;
-    int n1[6]; //node_ordinals
-    int e1[7]; //edge_ordinals
 
-    int a = 1, b = 2, c = 3, d = 4, e = 5, f = 6;
-
-    n1[0] = g1.add_node(a);
-    n1[1] = g1.add_node(b);
-    n1[2] = g1.add_node(c);
-    n1[3] = g1.add_node(d);
-    n1[4] = g1.add_node(e);
-    n1[5] = g1.add_node(f);
-
-    e1[0] = g1.add_edge(n1[0], n1[1]);
-    e1[1] = g1.add_edge(n1[0], n1[3]);
-    e1[2] = g1.add_edge(n1[4], n1[5]);
-    e1[3] = g1.add_edge(n1[2], n1[5]);
-    e1[4] = g1.add_edge(n1[2], n1[3]);
-    e1[5] = g1.add_edge(n1[2], n1[5]);
-
-    std::map<int, int> nodemap = lib::is_bipartite(g1);
-    for (auto it = nodemap.begin(); it != nodemap.end(); it++)
-        std::cout << (*it).first << " => " << (*it).second << std::endl;
-}
-
-void test_notbipartite_undigraph() {
-    std::cout << "Testing undirected graph bipartite\n";
-    lib::UndirectedGraph<int> g1;
-    int n1[6]; //node_ordinals
-    int e1[7]; //edge_ordinals
-
-    int a = 1, b = 2, c = 3, d = 4, e = 5, f = 6;
-
-    n1[0] = g1.add_node(a);
-    n1[1] = g1.add_node(b);
-    n1[2] = g1.add_node(c);
-    n1[3] = g1.add_node(d);
-    n1[4] = g1.add_node(e);
-    n1[5] = g1.add_node(f);
-
-    e1[0] = g1.add_edge(n1[0], n1[1]);
-    e1[1] = g1.add_edge(n1[0], n1[4]);
-    e1[2] = g1.add_edge(n1[1], n1[2]);
-    e1[3] = g1.add_edge(n1[1], n1[4]);
-    e1[4] = g1.add_edge(n1[2], n1[3]);
-    e1[5] = g1.add_edge(n1[3], n1[4]);
-
-    std::map<int, int> nodemap = lib::is_bipartite(g1);
-    for (auto it = nodemap.begin(); it != nodemap.end(); it++)
-        std::cout << (*it).first << " => " << (*it).second << std::endl;
-}
 
 void test_attributes() {
     lib::Graph<int> g1;
@@ -421,8 +367,8 @@ int main() {
     test_bft_digraph();
     test_bft_undigraph();
     test_bipartite_digraph();
-    test_bipartite_undigraph();
-    test_notbipartite_undigraph();
+    //test_bipartite_undigraph();
+    //test_notbipartite_undigraph();
     test_min_spanning_tree();
     test_graph_import_export();
 
