148514eec33ad3e3490e4d1cc511bddf6814f9f1
shiva <shiva@shiva-ThinkPad-E520.(none)>
2012-12-05 19:45:42 -0600

    formatting

--- include/exceptions.h
+++ include/exceptions.h
@@ -3,22 +3,27 @@
  * @author: GraphLib Team
  * Contains the exception classes used by the Graph Library.
  */
+
 #include<exception>
 #include<string>
 #ifndef EXCEPTIONS_H
 #define EXCEPTIONS_H
 
-class InvalidAccessException : public std::exception {
-	std::string msg;
+class InvalidAccessException: public std::exception {
+    std::string msg;
+
 public:
-	InvalidAccessException(std::string message) {
-		msg = message;
-	}
-	InvalidAccessException() {
-		msg = "Invalid access";
-	}
-	virtual const char* what() const throw() {
-		return (char *)msg.c_str();
-	}
+
+    InvalidAccessException(std::string message) {
+        msg = message;
+    }
+
+    InvalidAccessException() {
+        msg = "Invalid access";
+    }
+
+    virtual const char* what() const throw () {
+        return (char *) msg.c_str();
+    }
 };
 #endif
